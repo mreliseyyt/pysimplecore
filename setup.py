b55b40438e26395b021d9ae3bd0d1d7cd23ab711
MrEliseyYT <94438995+mreliseyyt@users.noreply.github.com>
2026-06-21 15:00:59 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="simplecore",
+    name="pysimplecore",
     version="0.0.1",
     author="mreliseyyt",
     author_email="mail@mreliseyyt.ru",
     description="Легковесная утилитарная библиотека для Python",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/mreliseyyt/simplecore",
+    url="https://github.com/mreliseyyt/pysimplecore",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
